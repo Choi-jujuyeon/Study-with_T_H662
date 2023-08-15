bc966a72db80706730462f7daea94e0d2ca64439
Choi-jujuyeon <chlwndus1216@naver.com>
2023-08-15 14:26:26 +0900

    create-li-tag

--- review-english-2/src/app/page.tsx
+++ review-english-2/src/app/page.tsx
@@ -1,5 +1,20 @@
 import { NextPage } from "next";
 const Home: NextPage = () => {
-    return <main className="bg-red-100">Home</main>;
+    return (
+        <main className="bg-red-100 ">
+            <div>Review English</div>
+            <ul>
+                <li>
+                    <button>Day 1</button>
+                </li>
+                <li>
+                    <button>Day 2</button>
+                </li>
+                <li>
+                    <button>Day 3</button>
+                </li>
+            </ul>
+        </main>
+    );
 };
 export default Home;
