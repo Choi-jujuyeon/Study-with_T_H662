b6e29aded713963cf922dfe624d7fe802854a5a7
Choi-jujuyeon <chlwndus1216@naver.com>
2023-08-15 14:41:12 +0900

    map()

--- review-english-2/src/app/page.tsx
+++ review-english-2/src/app/page.tsx
@@ -1,14 +1,15 @@
 import DayCard from "@/components/DayCards";
 import { NextPage } from "next";
+import data from "@/data.json";
 
 const Home: NextPage = () => {
     return (
         <main className="py-24 px-12 min-h-screen flex flex-col justify-start items-center">
             <div className="font-semibold">Review English</div>
             <ul className="mt-12 w-full grid grid-cols-4 gap-4">
-                <DayCard index={0} />
-                <DayCard index={1} />
-                <DayCard index={2} />
+                {data.map((_, i) => (
+                    <DayCard key={i} index={i} />
+                ))}
             </ul>
         </main>
     );
