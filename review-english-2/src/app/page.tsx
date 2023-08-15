943ce70d6da35835979841e64c8ef3d17fcbccff
Choi-jujuyeon <chlwndus1216@naver.com>
2023-08-15 14:33:26 +0900

    create-component

--- review-english-2/src/app/page.tsx
+++ review-english-2/src/app/page.tsx
@@ -1,3 +1,4 @@
+import DayCard from "@/components/DayCards";
 import { NextPage } from "next";
 
 const Home: NextPage = () => {
@@ -5,15 +6,9 @@ const Home: NextPage = () => {
         <main className="py-24 px-12 min-h-screen flex flex-col justify-start items-center">
             <div className="font-semibold">Review English</div>
             <ul className="mt-12 w-full grid grid-cols-4 gap-4">
-                <li className="text-center border-black border-2 font-medium rounded-lg shadow-md shadow-gray-300">
-                    <button>Day 1</button>
-                </li>
-                <li className="text-center border-black border-2 font-medium rounded-lg shadow-md shadow-gray-300">
-                    <button>Day 2</button>
-                </li>
-                <li className="text-center border-black border-2 font-medium rounded-lg shadow-md shadow-gray-300">
-                    <button>Day 3</button>
-                </li>
+                <DayCard />
+                <DayCard />
+                <DayCard />
             </ul>
         </main>
     );
---
+++ review-english-2/src/components/DayCards.tsx
@@ -0,0 +1,10 @@
+import { FC } from "react";
+
+const DayCard: FC = () => {
+    return (
+        <li className="text-center border-black border-2 font-medium rounded-lg shadow-md shadow-gray-300">
+            <button>Day 3</button>
+        </li>
+    );
+};
+export default DayCard;
